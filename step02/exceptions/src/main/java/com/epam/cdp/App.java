9849fe55e1b3a5497ac35be7cf4a5ee06ef582ca
Scepion1d <scepion1d@Scepion1ds-MacBook-Pro.local>
2021-06-07 23:17:06 +0200

    [new] Added exceptions handling

---
+++ step02/exceptions/src/main/java/com/epam/cdp/App.java
@@ -0,0 +1,47 @@
+package com.epam.cdp;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        try {
+            out_of_mem_easy();
+        } catch (OutOfMemoryError error) {
+            System.out.format("OutOfMemory: %s\n", error.getMessage());
+        }
+
+        try {
+            out_of_mem_hard();
+        } catch (OutOfMemoryError error) {
+            System.out.format("OutOfMemory: %s\n", error.getMessage());
+        }
+
+        try {
+            stack_overflow();
+        } catch (StackOverflowError error) {
+            System.out.format("StackOverflowError: %s\n", error.getMessage());
+        }
+    }
+
+    private static void out_of_mem_easy() {
+        long[] array = new long[Integer.MAX_VALUE];
+    }
+
+    private static void out_of_mem_hard() {
+        List<long[]> list = new LinkedList<>();
+
+        while (true) {
+            list.add(new long[65536]);
+        }
+    }
+
+    private static void stack_overflow() {
+        stack_overflow();
+    }
+}
