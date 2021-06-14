865df529ffe4bff6b41e842c9dc4f3ed0991abde
Scepion1d <scepion1d@Scepion1ds-MacBook-Pro.local>
2021-06-15 00:05:43 +0200

    [new] Added statistic app

---
+++ step02/stat/src/main/java/com/epam/cdp/App.java
@@ -0,0 +1,19 @@
+package com.epam.cdp;
+
+import com.epam.cdp.stat.Statistic;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            new Statistic(args).printAll();
+            Thread.sleep(5000);
+        }
+    }
+}
