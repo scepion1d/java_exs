0964c7e7fb92b443d0ae0577764ed9a605acb7dc
Scepion1d <scepion1d@Scepion1ds-MacBook-Pro.local>
2021-07-06 18:55:36 +0200

    [new] Added array of random number processing

---
+++ step03/src/main/java/com/epam/cdp/App.java
@@ -0,0 +1,15 @@
+package com.epam.cdp;
+
+import com.epam.cdp.array.Array;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Array.print(1000);
+    }
+}
---
+++ step03/src/main/java/com/epam/cdp/array/Array.java
@@ -0,0 +1,48 @@
+package com.epam.cdp.array;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+@Slf4j
+public class Array {
+    private final Vector<Double> elements;
+    private final int size;
+
+    public static void print(int size) {
+        Array array = new Array(size);
+        log.info("Max: {}", array.maxElement());
+        log.info("Min: {}", array.minElement());
+        log.info("Average: {}", array.avgElement());
+        log.info("Max 5: {}", array.maxElements(5));
+    }
+
+    private Array(int size) {
+        elements = new Vector<>();
+        this.size = size;
+        init();
+    }
+
+    public Double maxElement() {
+        return elements.get(0);
+    }
+
+    public Double minElement() {
+        return elements.get(size - 1);
+    }
+
+    public Double avgElement() {
+        return elements.stream().mapToDouble(x -> x).average().orElse(0.0);
+    }
+
+    public List<Double> maxElements(int number) {
+        return elements.subList(0, number);
+    }
+
+    private void init() {
+        for (int i = 0; i < size; i++) {
+            elements.add(Math.random());
+        }
+        Collections.sort(elements, Collections.reverseOrder());
+    }
+}
